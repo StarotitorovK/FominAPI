884ea791bd0dabe1398b34245cfa1cdb7ec804da
Alexaner <107759041+InvincibleChance@users.noreply.github.com>
2024-09-20 00:52:58 +0300

    Update TF-IDF.py

--- TF-IDF.py
+++ TF-IDF.py
@@ -20,27 +20,27 @@ from nltk.corpus import stopwords
 import pandas as pd
 import time
 
-# Подгрузка токенов из файла
+
 def load_tokens_from_file(file_name):
     with open(file_name, 'r', encoding='utf-8') as file:
         return json.load(file)
 
-# Загрузка стоп-слов для русского языка
+
 russian_stopwords = stopwords.words('russian')
 
-# Загрузка обработанных статей (токенов) из файла
-processed_articles = load_tokens_from_file("tokens.json")
-article_links = list(processed_articles.keys())  # Ссылки на статьи
-article_texts = list(processed_articles.values())  # Обработанные тексты статей (токены)
 
-# Создание TF-IDF матрицы на основе обработанных текстов
+processed_articles = load_tokens_from_file("tokens.json")
+article_links = list(processed_articles.keys())  
+article_texts = list(processed_articles.values())  
+
+
 vectorizer = TfidfVectorizer(tokenizer=word_tokenize, stop_words=russian_stopwords)
 X = vectorizer.fit_transform(article_texts)
 
-# Запрос пользователя
-user_query = "Можно ли забеременеть во время месячных?"
 
-# Предобработка запроса пользователя (аналогично статьям)
+user_query = ""
+
+
 def preprocess_text(text):
     stemmer = SnowballStemmer("russian")
     text = text.lower()
@@ -56,7 +56,7 @@ query_vector = vectorizer.transform([processed_query])
 
 similarities = cosine_similarity(query_vector, X)
 
-# Вывод статей с высоким уровнем совпадения
+
 print("Статьи с высоким уровнем совпадения (> 0.08):")
 for idx, similarity in enumerate(similarities[0]):
     if similarity > 0.08:
