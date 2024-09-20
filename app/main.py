2b23b266858bbb1b4315b9f3d75b54e676a74ef5
StarotitorovK <kirjastar@rambler.ru>
2024-09-21 01:52:42 +0300

    Add processinig by ArticleSearch to GET request on /health

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from scripts.search_engine import ArticleSearch
 
+app = FastAPI()
+search_engine = ArticleSearch("C:/Users/Пользователь/OneDrive/Рабочий стол/FominAPI/tokens.json")
 
 @app.get("/health")
 def get_article(question: str):
-    return {"answer": "I'm alive! You asked: " + question}
+    answer = search_engine.find_articles(question)
+    return {"answer": answer}
 
 
 @app.get("/faq")
